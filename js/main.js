43f59a75ca916e0f530926a32a685bb7d4130cd8
oljuanfe <ojuanferrero@gmail.com>
2018-06-29 18:26:13 +0200

    Create html and functions

--- js/main.js
+++ js/main.js
@@ -1,3 +1,40 @@
 'use strict';
 
 console.log('>> Ready :)');
+
+function getRandomNumber(max) {
+  return Math.ceil(Math.random() * max);
+}
+
+// console.log('> ' + getRandomNumber(100));
+var randomNumber = getRandomNumber(100);
+console.log(randomNumber);
+
+var button = document.querySelector('.testButton');
+var number = document.querySelector('.number');
+console.log('<'+insideInput);
+
+function numberInsideInput(event){
+ var insideInput = number.value;
+ console.log('<'+insideInput);
+}
+
+button.addEventListener('click',numberInsideInput);
+
+var insideInput =number.value;
+
+console.log('<'+insideInput);
+
+var clues = document.querySelector('.clues');
+
+function compareNumber(){
+  console.log(insideInput);
+  if (insideInput===randomNumber){
+    clues.innerHTML = 'HAS GANADO, CAMPEONA';
+  }else if(insideInput<randomNumber){
+    clues.innerHTML = 'Demasiado pequeño';
+  }else if(insideInput>randomNumber){
+    clues.innerHTML = 'Demasiado alto';
+  }
+}
+compareNumber();
