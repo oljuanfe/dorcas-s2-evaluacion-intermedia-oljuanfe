5df67d8c2f7c8319edcb33b8df299a01fe6e6697
oljuanfe <ojuanferrero@gmail.com>
2018-07-04 11:32:22 +0200

    Create functional compareNumber function

--- js/main.js
+++ js/main.js
@@ -7,36 +7,57 @@ function getRandomNumber(max) {
 }
 
 // console.log('> ' + getRandomNumber(100));
+//Obtener numero aleatorio y guardarlo en variable
+
 var randomNumber = getRandomNumber(100);
 console.log(randomNumber);
 
+//Apuntar hacia el boton y el input
+
 var button = document.querySelector('.testButton');
 var number = document.querySelector('.number');
-console.log('<'+insideInput);
+var clues = document.querySelector('.clues');
+
+// var insideInputAsAString = number.value;
+//  var insideInput = parseInt(insideInputAsAString);
+//  console.log('1<<<'+insideInput);
+
+//Funcion para comparar numero aleatorio con numero input
+//pongo recogida de input dentro funcion para que compare nuevo numero nuevo cada vez que se ejecute
+
+function compareNumber(){
+  var insideInputAsAString = number.value;
+  var insideInput = parseInt(insideInputAsAString);
+  if (insideInput===randomNumber){
+    clues.innerHTML = 'HAS GANADO, CAMPEONA';
+    console.log('HAS GANADO, CAMPEONA');
+  }else if(insideInput<randomNumber){
+    clues.innerHTML = 'Demasiado pequeño';
+    console.log('Demasiado pequeño');
+  }else if(insideInput>randomNumber){
+    clues.innerHTML = 'Demasiado alto';
+    console.log('Demasiado alto');
+  }
+  console.log('funcion comparar');
+}
+
+//Funcion para guardar el valor introducido en input
 
 function numberInsideInput(event){
- var insideInput = number.value;
-  var insideInputNumber = parseInt(insideInput);
- console.log('<'+insideInput);
+ // var insideInputAsAString = number.value;
+ //  var insideInput = parseInt(insideInputAsAString);
+  // console.log('1<<<'+insideInput);
+  compareNumber();
 }
+
+//Evento click cuando metemos valor input
 
 button.addEventListener('click',numberInsideInput);
 
-var insideInput =number.value;
+// var insideInput =number.value;
+//
+// console.log('3<'+insideInput);
 
-console.log('<'+insideInput);
 
-var clues = document.querySelector('.clues');
-
-function compareNumber(){
-  console.log(insideInput);
-  if (insideInput===randomNumber){
-    clues.innerHTML = 'HAS GANADO, CAMPEONA';
-  }else if(insideInput<randomNumber){
-    clues.innerHTML = 'Demasiado pequeño';
-  }else if(insideInput>randomNumber){
-    clues.innerHTML = 'Demasiado alto';
-  }
-}
-
-button.addEventListener('click',compareNumber);
+//
+// button.addEventListener('click',compareNumber);
